d8d261aec7c3f45492fa61f2311c4a8c15e7b79e
Rohitsinh Sonawane <rohitsinh.sonawane@gmail.com>
2026-03-14 19:15:52 +0530

    Updated DbContext due to system conflict

---
+++ SupportTicketSystem.Api/Data/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SupportTicketSystem.Api.Entities;
+
+
+namespace SupportTicketSystem.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<TicketComment> TicketComments { get; set; }
+        public DbSet<TicketStatusHistory> TicketStatusHistories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Ticket>()
+                .Property(t => t.Status)
+                .HasConversion<string>();
+
+            modelBuilder.Entity<Ticket>()
+                .Property(t => t.Priority)
+                .HasConversion<string>();
+        }
+    }
+}
